4cb3e55353cc824f8a6e6a1e80887ae52ef8fd01
SunilYeggoni123 <95092264+SunilYeggoni123@users.noreply.github.com>
2021-11-30 14:34:45 +0530

    Java file

---
+++ sample.ts
@@ -0,0 +1,31 @@
+class Person {
+    constructor(private firstName: string, private lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    getFullName(): string {
+        return `${this.firstName} ${this.lastName}`;
+    }
+    describe(): string {
+        return `This is ${this.firstName} ${this.lastName}.`;
+    }
+}
+
+class Employee extends Person {
+    constructor(
+        firstName: string,
+        lastName: string,
+        private jobTitle: string) {
+        
+       
+        super(firstName, lastName);
+    }
+    
+}
+
+let emp = new Employee('John','Doe','Front-end Developer');
+
+
+
+console.log(emp.getFullName());
+console.log(emp.describe());
